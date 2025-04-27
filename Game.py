849b84153c164e45bc9fe6b51e4e261a9788ea29
Pintu Pandey <psp169142021@gmail.com>
2025-04-27 16:45:41 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,14 @@
+# Madlib game
+# word game where you Create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description)")
+noun1 = input("Enter a noun (person, place, thing:)")
+adjective2 = input("Enter an adjective (description)")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description)")
+
+print(f"Today I went to a {adjective1}.")
+print(f"In an exibit, I saw a{noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
